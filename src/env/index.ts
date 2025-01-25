2211be560f0f645282abba6984038f8ad1361fce
zielisgabriel <josegabrielas0586@gmail.com>
2025-01-25 04:46:36 -0300

    add url() on env validation

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,7 @@ import z from 'zod'
 
 const envSchema = z.object({
     PORT: z.string().default('3333'),
-    DATABASE_URL: z.string(),
+    DATABASE_URL: z.string().url(),
     JWT_SECRET: z.string(),
 })
 
