e31de28f2afa1f986f43dc9760dbe3ac1cf448ee
zielisgabriel <josegabrielas0586@gmail.com>
2025-01-25 05:26:17 -0300

    verify user authorization added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express'
 import { userRoutes } from './users.routes'
 import { sessionsRoutes } from './sessions.routes'
-import { ensureAuthenticated } from '../middlewares/ensure-authenticated'
 
 const routes = Router()
 
-routes.use('/users', ensureAuthenticated, userRoutes)
-routes.use('/sessions', ensureAuthenticated, sessionsRoutes)
+routes.use('/users', userRoutes)
+routes.use('/sessions', sessionsRoutes)
 
 export { routes }
