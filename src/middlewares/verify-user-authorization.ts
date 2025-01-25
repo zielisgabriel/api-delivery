053a7eddb08a3cde43f3307805d40b9ccb7cc866
zielisgabriel <josegabrielas0586@gmail.com>
2025-01-25 05:21:36 -0300

    middleware verify user authorization added

---
+++ src/middlewares/verify-user-authorization.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express"
+import { AppError } from "../utils/AppError"
+
+export function verifyUserAuthorization(role: string[]){
+    return (req: Request, res: Response, next: NextFunction) => {
+        
+        if(!req.user || !role.includes(req.user.role)){
+            throw new AppError('Não autorizado!')
+        }
+
+        return next()
+    }
+}
