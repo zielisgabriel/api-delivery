dd0c38a08ab60f25b05ba26288ff791f0f652d4f
zielisgabriel <josegabrielas0586@gmail.com>
2025-01-25 01:50:18 -0300

    prisma config added

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -3,18 +3,20 @@ import z from 'zod'
 
 export class UserController{
     async index(req: Request, res: Response){
+        
+    }
+
+    async create(req: Request, res: Response){
         const bodySchema = z.object({
-            name: z.string().trim().min(6),
-            email: z.string().trim().min(10),
-            password: z.string().trim().min(8)
+            name: z.string().trim().min(6, 'O nome tem que ter pelo menos 6 caracteres'),
+            email: z.string().trim().min(10, 'O email tem que ter pelo menos 10 caracteres'),
+            password: z.string().trim().min(8, 'A senha tem que ter pelo menos 8 caracteres')
         })
 
         const { name, email, password } = bodySchema.parse(req.body)
 
-        res.json({ message: 'ok' })
-    }
+        res.json({ name, email, password })
 
-    async create(req: Request, res: Response){
-        res.status(201).json({ message: 'create' })
+        return res.status(201).json({ message: 'create' })
     }
 }
