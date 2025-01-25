79dc6dc0f93ac1b38012290d2279da2093298eb4
zielisgabriel <josegabrielas0586@gmail.com>
2025-01-25 00:55:02 -0300

    new user creation route added

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express'
 
 export class UserController{
-    index(req: Request, res: Response){
+    async index(req: Request, res: Response){
         res.json({ message: 'ok' })
+    }
+
+    async create(req: Request, res: Response){
+        res.status(201).json({ message: 'create' })
     }
 }
--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,9 +1,14 @@
 import { Request, Response, NextFunction } from 'express'
 import { AppError } from '../utils/AppError'
+import { ZodError } from 'zod'
 
 export function errorHandling(error: any, req: Request, res: Response, _: NextFunction){
     if(error instanceof AppError){
         return res.status(error.statusCode).json({ message: error.message })
+    }
+
+    if(error instanceof ZodError){
+        return res.status(500).json({ message: error.message })
     }
 
     return res.status(500).json({ message: error.message })
