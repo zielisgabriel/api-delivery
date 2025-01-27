8efd39c2dc4fc0ddfcfe6ed2b184e70f38a803af
zielisgabriel <josegabrielas0586@gmail.com>
2025-01-26 23:23:13 -0300

    update on environment variable using zod

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import z from 'zod'
 
 const envSchema = z.object({
-    PORT: z.string().default('3333'),
+    PORT: z.coerce.number().default(3333),
     DATABASE_URL: z.string().url(),
     JWT_SECRET: z.string(),
 })
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 import { ENV } from './env'
 
-const PORT = Number(ENV.PORT)
+const PORT = ENV.PORT
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
